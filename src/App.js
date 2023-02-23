47193834ebe8c44dcd1262444602bf11a03d21f8
fossheim <fossheimjoneven@gmail.com>
2023-02-23 14:36:06 +0100

    feat:added conditional rendering for class and page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import Card from './components/pokemon/Card';
 import './styles/main.css';
 
 const App = () => {
+  let isDay = true;
+
+  if (!isDay) {
+    return <h1>Come Back During The Day</h1>;
+  }
+
   return (
     <>
+      <h1>{isDay ? 'God Dag' : 'God Kveld'}</h1>
       {pkmn.map((pokemon) => {
         return (
           <Card
--- src/components/pokemon/Card.js
+++ src/components/pokemon/Card.js
@@ -2,8 +2,10 @@ import AltNames from './AltNames';
 import Types from './Types';
 import Stats from './Stats';
 const Card = ({ name, type, base }) => {
+  let typeClass = type[0].toLowerCase();
+
   return (
-    <div className='card'>
+    <div className={'card ' + typeClass}>
       <h1>{name.english}</h1>
       <AltNames name={name} />
       <Types type={type} />
